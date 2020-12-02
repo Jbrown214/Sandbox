3a72243d15206fff16672cc67e0472748aa157eb
Jbrown214 <palonzo54@aol.com>
2020-12-02 17:53:05 -0600

    Add files via upload

---
+++ Amazon automation.py
@@ -0,0 +1,87 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.support.ui import Select
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import StaleElementReferenceException
+from selenium.webdriver.common.action_chains import ActionChains 
+import selenium.webdriver.support.ui as ui
+from selenium.webdriver.common.keys import Keys
+
+ignored_exceptions = (NoSuchElementException,StaleElementReferenceException,)
+
+driver = webdriver.Chrome(ChromeDriverManager().install())
+driver.get('https://www.amazon.com/')
+driver.maximize_window()
+driver.implicitly_wait(15)
+wait = WebDriverWait(driver, 10)
+item = input(str("Enter the item you want to search: \n".replace('/','')))
+
+# this function takes the above user inputs and searches item and filters it
+def find_product(option):
+    search = driver.find_element_by_xpath("""/html/body/div[1]
+    /header/div/div[1]/div[2]/div/form/div[2]/div[1]/input""").send_keys( option + '\n')
+
+    sort_by = input("""If you want to sort by featured product, press 1. 
+    \n If you want to sort by price , press 2. 
+    \n If you want to sort by custmer review, press 3.
+    \n""")
+
+    driver.implicitly_wait(15)
+    wait = WebDriverWait(driver, 10)
+    sel = Select(driver.find_element_by_xpath("""/html/body/div[1]/div[2]/span
+    /div/span/h1/div/div[2]/div/div/form/span/select"""))
+
+    driver.implicitly_wait(15)
+    wait = WebDriverWait(driver, 10) 
+
+    if sort_by == str(1):
+        sort_value = sel.select_by_visible_text("Featured")
+        time.sleep(0.8)
+    elif sort_by == str(2):
+        sort_value = sel.select_by_visible_text("Price: Low to High")
+        time.sleep(0.8)
+    elif sort_by == str(3):
+        sort_value = sel.select_by_visible_text("Avg. Customer Review")
+
+
+        time.sleep(0.8)
+
+def product_click(): # this function clicks on every other image within the results page and clicks items into new tabs to be viewed by user
+    
+    elems = driver.find_elements_by_xpath("""//img[@src]""")
+    # elems = driver.find_elements_by_xpath("//div[@data-asin]")
+    i = 1
+    cw = driver.current_window_handle
+    for elem in elems[i:14:2]:    
+        try:  
+            tw = ActionChains(driver).context_click(elem).key_down(Keys.CONTROL).click(elem).perform()
+            time.sleep(0.8)
+            i += 1
+        except StaleElementReferenceException:
+            continue
+
+    tabs = driver.window_handles
+    return cw, tabs
+
+def review_tabs(result_tab,open_tabs): # this function reviews each tab that is open, scrolls down the page, and closes the tab after each view
+    for x in range(len(open_tabs)):
+        if open_tabs[x] != result_tab:
+            driver.switch_to.window(open_tabs[x])
+            driver.execute_script("window.scrollTo (0, 540)")
+            time.sleep(0.8)
+            driver.close()
+        else:
+            driver.close()
+
+if __name__ == "__main__": 
+    product = find_product(option=item)
+    results_page = product_click()
+    tab_pages = review_tabs(results_page[0],results_page[1])
+
+
+
